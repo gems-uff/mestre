f7abf1ab6a196d07ca9686b93d867fe7510f9065
helenocampos <heleno_scj@hotmail.com>
2021-04-08 19:45:18 -0300

    Changes the collect_attributes_db script to extract the data from the repos and not from the API

--- scripts/collect_attributes_db.py
+++ scripts/collect_attributes_db.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-from github import Github
 import pandas as pd
-import base64
-import urllib.request
 import time
 import datetime
 import configs
 import database
 import traceback
-
-githubTokens = []
-with open('github_keys') as file:
-    githubTokens = [line.rstrip('\n') for line in file]
-
-g=Github(githubTokens[0])
+import pathlib
+import os
+import subprocess
 
 def getCyclomaticComplexity(code):
     ifs = code.count("if")
@@ -91,6 +85,19 @@ def getRightChunkCode(chunkCode):
             rightChunk+=line+"\n"
     return rightChunk    
 
+def execute_command(command):
+    try:
+        my_env = os.environ.copy()
+        result = subprocess.check_output([command], stderr=subprocess.STDOUT, text=True, shell=True, env=my_env)
+        return result
+    except subprocess.CalledProcessError as e:
+        print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output), flush=True)
+    return ''
+
+def get_file_content(commitSHA, file_path):
+    command = f"git show {commitSHA}:{file_path}"
+    print(command)
+    return execute_command(command)
 
 
 df = pd.read_csv(configs.INITIAL_DATASET_PATH)
@@ -100,26 +107,21 @@ data = []
 start_time = time.time()
 counter = 0
 print_every = 20
-token_index=0
-g=Github(githubTokens[token_index])
-token_index+=1
+
+starting_folder = pathlib.Path(__file__).parent.absolute()
 print("Processing start at %s" % (datetime.datetime.now()))
-for index, row in df.iterrows():
-    requestsRemaining = g.rate_limiting[0]
-    if(requestsRemaining>2):
-        row2 = []
-        url = f"https://api.github.com/repos/{row['project']}"
-        beginLine, endLine = database.get_conflict_position(row['chunk_id'])
-        sha = row['sha']
-        repoName = row['project']
-        if(repoName in repos):
-            repo = repos.get(repoName)
-        else:
-            repo = g.get_repo(repoName)
-            repos[repoName] = repo
-        try:   
-            file = repo.get_contents(row['path'].replace(row['project'],''), ref=sha)
-            fileContent = base64.b64decode(file.content).decode("utf-8")
+grouped_df = df.groupby('project')
+for group_name, df_group in grouped_df:
+    for index, row in df_group.iterrows():
+        project_folder = f"{configs.REPOS_PATH}/{row['project']}"
+        if os.path.exists(project_folder):
+            os.chdir(project_folder)
+            row2 = []
+            beginLine, endLine = database.get_conflict_position(row['chunk_id'])
+            sha = row['sha']
+            repoName = row['project']
+            file_path = row['path'].replace(row['project']+"/", '')
+            fileContent = get_file_content(sha, file_path)
             chunkContent = getChunkContent(row['chunk_id'])
             fileSize = getFileSize(fileContent)
             chunkSize = getFileSize(chunkContent)
@@ -146,27 +148,18 @@ for index, row in df.iterrows():
                 percentage = (index/size)*100
                 intermediary_time = time.time() - start_time
                 estimated = ((intermediary_time * 100)/percentage)/60/60
-                print('{} --- {:.2f}% done... estimated time to finish: {:.2f} hours. {} of {} rows processed. {} requests '.format(datetime.datetime.now(),percentage, estimated, index, size, requestsRemaining), end="\r")
+                print('{} --- {:.2f}% done... estimated time to finish: {:.2f} hours. {} of {} rows processed.'.format(datetime.datetime.now(),percentage, estimated, index, size))
                 counter = 0
             counter = counter+1
             data.append(row2)
-        except (Exception) as error:
-            traceback.print_exc()
-            print(f'main: {error}')
+        os.chdir(starting_folder)
+            
 
-    else:
-        print("Github API requests exhausted. Waiting 60 seconds and trying again....", end="\r")
-        time.sleep(60)
-        if(token_index < len(githubTokens)-1):
-            g=Github(githubTokens[token_index])
-            token_index+=1
-        else:
-            token_index=0
 elapsed_time = time.time() - start_time
+
 print()
 print("Processed in %d seconds. Exporting csv..." % (elapsed_time))
 df2 = pd.DataFrame(data, columns = ['chunk_id','leftCC', 'rightCC', 'fileCC', 'chunkAbsSize', 'chunkRelSize', 'chunkPosition'])
 result_df = pd.merge(df,df2, on='chunk_id')
 result_file = f"{configs.DATA_PATH}/collected_attributes2.csv"
 result_df.to_csv(result_file, index=False)
-
