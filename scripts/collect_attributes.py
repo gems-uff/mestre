40a800e4ac83c9aaca41630e8b90cde9cc46528d
helenocampos <heleno_scj@hotmail.com>
2021-04-30 10:41:00 -0300

    Fixes a bug in collect_attributes script. Also adds a mecanism for restarting the process from where it stopped

--- scripts/collect_attributes.py
+++ scripts/collect_attributes.py
@@ -5,6 +5,7 @@ import os
 import time
 import sys
 import pathlib
+import sys
 from datetime import datetime
 
 # Keywords count in commit messages: fix, bug, feature, improve, document, refactor, update, add, remove, use, delete, and change.
@@ -70,7 +71,12 @@ def get_number_changed_lines(mergeSHA, parentSHA):
 def get_commit_date(SHA):
     command = f"git show -s --format=%ci {SHA}"
     output = execute_command(command).strip()
-    date = datetime.strptime(output, '%Y-%m-%d %H:%M:%S %z') 
+    output = output.split('\n')
+    if len(output) > 0:
+        output = output[0] # takes only the date and discard possible warnings
+        date = datetime.strptime(output, '%Y-%m-%d %H:%M:%S %z') 
+    else:
+        date = None
     return date
 
 
@@ -80,12 +86,16 @@ def get_commit_date(SHA):
 def get_conclusion_delay(parent1SHA, parent2SHA):
     parent1_date = get_commit_date(parent1SHA)
     parent2_date = get_commit_date(parent2SHA)
-    if(parent1_date > parent2_date):
-        difference = parent1_date - parent2_date
+    if parent1_date != None and parent2_date!= None:
+        if(parent1_date > parent2_date):
+            difference = parent1_date - parent2_date
+        else:
+            difference = parent2_date - parent1_date
+        difference = difference.days
     else:
-        difference = parent2_date - parent1_date
+        difference = -1
     # print(f'parent1: {parent1_date}  | parent2: {parent2_date}  | difference: {difference.days}')
-    return difference.days
+    return difference
     
 
 
@@ -118,55 +128,84 @@ def write_failed_chunks(failed):
         for failed_chunk in failed:
             file.write(f"{failed_chunk[0]}:{failed_chunk[1]}\n")
 
-def main():
-    df = pd.read_csv(configs.INITIAL_DATASET_PATH, encoding="utf-8")
-    starting_folder = pathlib.Path(__file__).parent.absolute()
+def load_existent_data():
+    df = pd.read_csv(f'{configs.DATA_PATH}/collected_attributes1.csv', encoding="utf-8")
     collected_commits = set()
     extracted_data = []
-    print(f'Starting the collection process for {len(df)} chunks...')
-    config_limits()
-    current_index = 0
+    failed_chunks = []
+    for index, row in df.iterrows():
+        extracted_data.append(row.tolist())
+        collected_commits.add(f"{row['project']}-{row['sha']}")
+
+    with open('failed_chunks.txt', 'r') as file:
+        for line in file:
+            if line != '':
+                chunk_id, failure = line.strip().split(':')
+                failed_chunks.append([chunk_id, failure])
+    return collected_commits, extracted_data, failed_chunks
+
+def main(arguments):
+    chunk_start = 0
+    collected_commits = set()
+    extracted_data = []
+    failed_chunks = []
+
+    if len(arguments) == 2:
+        try:
+            chunk_start = int(arguments[1])
+            collected_commits, extracted_data, failed_chunks = load_existent_data()
+        except:
+            print('Invalid arguments. The only argument accepted is the chunk id to start the process from.')
+            exit()
+
+    df = pd.read_csv(configs.INITIAL_DATASET_PATH, encoding="utf-8")
+    starting_folder = pathlib.Path(__file__).parent.absolute()
+    print(f'Starting the collection process for {len(df)} chunks... Starting from chunk_id {chunk_start}')
+    current_index = len(extracted_data)
     columns = ['chunk_id', 'sha', 'project', 'left_lines_added', 'left_lines_removed', 'right_lines_added', 'right_lines_removed', 'conclusion_delay']
     columns.extend(['keyword_fix', 'keyword_bug', 'keyword_feature', 'keyword_improve', 'keyword_document', 'keyword_refactor', 'keyword_update'])
     columns.extend(['keyword_add', 'keyword_remove', 'keyword_use', 'keyword_delete', 'keyword_change'])
     save_every = 50
-    failed_chunks = []
+
     for index, row in df.iterrows():
         current_index +=1
         status = (current_index / len(df)) * 100
-        print(f"{time.ctime()} ### {status:.1f}% of chunks processed. Processing chunk {row['chunk_id']} for project: {row['project']}", flush=True)
-        commit_index = f"{row['project']}-{row['sha']}"
-        if(commit_index not in collected_commits): # since these attributes are related to the commit and not to the chunk, we only collect it if the merge commit (sha) is not already collected
-            data = []
-            data.append(row['chunk_id'])
-            data.append(row['sha'])
-            data.append(row['project'])
-            project_folder = f"{configs.REPOS_PATH}/{row['project']}"
-            if os.path.exists(project_folder):
-                os.chdir(project_folder)
-                if not check_commits_consistency([row['sha'], row['leftsha'], row['rightsha'], row['basesha']]):
-                    failed_chunks.append([row['chunk_id'], 'BAD_COMMIT'])
+        if row['chunk_id'] > chunk_start:
+            print(f"{time.ctime()} ### {status:.1f}% of chunks processed. Processing chunk {row['chunk_id']} for project: {row['project']}", flush=True)
+            commit_index = f"{row['project']}-{row['sha']}"
+            if(commit_index not in collected_commits): # since these attributes are related to the commit and not to the chunk, we only collect it if the merge commit (sha) is not already collected
+                data = []
+                data.append(row['chunk_id'])
+                data.append(row['sha'])
+                data.append(row['project'])
+                project_folder = f"{configs.REPOS_PATH}/{row['project']}"
+                if os.path.exists(project_folder):
+                    os.chdir(project_folder)
+                    if not check_commits_consistency([row['sha'], row['leftsha'], row['rightsha'], row['basesha']]):
+                        failed_chunks.append([row['chunk_id'], 'BAD_COMMIT'])
+                    else:
+                        config_limits()
+                        left_insertions, left_deletions = get_number_changed_lines(row['sha'], row['leftsha'])
+                        right_insertions, right_deletions = get_number_changed_lines(row['sha'], row['rightsha'])
+                        # print(f'left insertions: {left_insertions}  / left deletions: {left_deletions} / right insertions: {right_insertions}  /right deletions: {right_deletions}')
+                        conclusion_delay = get_conclusion_delay(row['leftsha'], row['rightsha'])
+                        keywords_frequency = get_keywords_frequency(row['leftsha'], row['rightsha'], row['basesha'])
+                        # print(f'conclusion delay: {conclusion_delay} | keywords frequency: {keywords_frequency}')
+                        data.extend([left_insertions, left_deletions, right_insertions, right_deletions,conclusion_delay])
+                        for keyword, frequency in keywords_frequency.items():
+                            data.append(frequency)
+                        collected_commits.add(commit_index)
+                        extracted_data.append(data)
+                    os.chdir(starting_folder)
                 else:
-                    left_insertions, left_deletions = get_number_changed_lines(row['sha'], row['leftsha'])
-                    right_insertions, right_deletions = get_number_changed_lines(row['sha'], row['rightsha'])
-                    # print(f'left insertions: {left_insertions}  / left deletions: {left_deletions} / right insertions: {right_insertions}  /right deletions: {right_deletions}')
-                    conclusion_delay = get_conclusion_delay(row['leftsha'], row['rightsha'])
-                    keywords_frequency = get_keywords_frequency(row['leftsha'], row['rightsha'], row['basesha'])
-                    # print(f'conclusion delay: {conclusion_delay} | keywords frequency: {keywords_frequency}')
-                    data.extend([left_insertions, left_deletions, right_insertions, right_deletions,conclusion_delay])
-                    for keyword, frequency in keywords_frequency.items():
-                        data.append(frequency)
-                    collected_commits.add(commit_index)
-                    extracted_data.append(data)
-                os.chdir(starting_folder)
-            else:
-                failed_chunks.append([row['chunk_id'], 'REPO_NOT_AVAILABLE'])
-        if current_index % save_every == 0:
-            pd.DataFrame(extracted_data, columns = columns).to_csv(f'{configs.DATA_PATH}/collected_attributes1.csv', index=False)
-            write_failed_chunks(failed_chunks)
+                    failed_chunks.append([row['chunk_id'], 'REPO_NOT_AVAILABLE'])
+            if current_index % save_every == 0:
+                print('Saving data collected so far...')
+                pd.DataFrame(extracted_data, columns = columns).to_csv(f'{configs.DATA_PATH}/collected_attributes1.csv', index=False)
+                write_failed_chunks(failed_chunks)
     os.chdir(starting_folder)
 
     pd.DataFrame(extracted_data, columns = columns).to_csv(f'{configs.DATA_PATH}/collected_attributes1.csv', index=False)
     write_failed_chunks(failed_chunks)
 
-main()
+main(sys.argv)
