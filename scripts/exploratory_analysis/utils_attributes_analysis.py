c0af8b4541f53a5ddb9a5957c3f59d3959c4ae53
helenocampos <heleno_scj@hotmail.com>
2021-05-17 21:50:34 -0300

    (WIP) updates attributes exploratory analysis notebook

--- scripts/exploratory_analysis/utils_attributes_analysis.py
+++ scripts/exploratory_analysis/utils_attributes_analysis.py
@@ -9,10 +9,10 @@ def get_figure_size(number_graphs, graphs_per_line):
     cols = graphs_per_line
     rows = int(math.ceil(N / cols))
 
-    length_x_axis = 30
-    length_y_axis = 15
+    length_x_axis = 32
+    length_y_axis = 28
 
-    fig_height = 12.
+    fig_height = 20.
 
     height = length_y_axis * rows
     width = length_x_axis  * cols
@@ -20,8 +20,7 @@ def get_figure_size(number_graphs, graphs_per_line):
     plot_aspect_ratio= float(width)/float(height)
     return (fig_height  * plot_aspect_ratio, fig_height )
 
-#TODO: remove counts for values with zero elements
-def count_plot_categorical(columns, df, grid_plots_per_line=2, log=False):
+def count_plot_categorical(columns, df, grid_plots_per_line=2, log=False, log_base=10):
     sns.set(font_scale=1)
     N = len(columns)
     cols = grid_plots_per_line
@@ -32,12 +31,23 @@ def count_plot_categorical(columns, df, grid_plots_per_line=2, log=False):
         column = columns[n]
         ax = fig.add_subplot(gs[n])
         ax.set_xticklabels(list(df[column].unique()), rotation=45)
-        g = sns.countplot(df[column], color='gray', ax=ax)
+        plt.xticks(
+            rotation=45, 
+            horizontalalignment='right',
+            fontweight='light',
+            fontsize='medium'  
+        )
+        labels = list(df[column].value_counts().reset_index(name="count").query("count > 0")["index"])
+        count = df[column].value_counts().reset_index(name="count").query("count > 0")
+        chart = count.plot(kind='bar', color='gray', legend=None, ax=ax)
+        chart.set_xticklabels(labels, rotation=45, horizontalalignment='right')
+        ax.set_title(column)
+        ax.set_ylabel(f'Count')
         if log:
-            g.set_yscale("log")
+            plt.yscale("log", base=log_base)
     fig.tight_layout()
 
-def hist_plot(columns, df, number_columns=2):
+def hist_plot(columns, df, number_columns=2, log=False, log_base=10):
     sns.set(font_scale=2)
 
     i = j = 0
@@ -46,6 +56,9 @@ def hist_plot(columns, df, number_columns=2):
     fig, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(30,30))
     for column in columns:
         df.hist(column=column, ax=ax[i][j], bins=25)
+        ax[i][j].set_ylabel(f'Count')
+        if log:
+            ax[i][j].set_yscale('log', basey=log_base)
         if j == cols-1:
             j = 0
             i+=1
@@ -64,15 +77,20 @@ def hist_plot_by_category(columns, df, by):
         g.fig.subplots_adjust(top=0.8)
 
 def violin_by_category(columns, df, category, number_columns=2):
-    sns.set(font_scale=2)
+    sns.set(font_scale=1.2)
     cols = number_columns
     rows = math.ceil(len(columns)/cols)
     fig = plt.figure(figsize=get_figure_size(len(columns), number_columns))
     gs = fig.add_gridspec(rows, cols)
     i = j = 0
+
+    new_df = df.copy()
+    new_df[category] = "Overall"
+    new_df = pd.concat([df,new_df])
+
     for column in columns:
         ax = fig.add_subplot(gs[i, j])
-        g = sns.violinplot(x=category, y=column, data=df)
+        g = sns.violinplot(x=category, y=column, data=new_df)
         g.set_xticklabels(g.get_xticklabels(),rotation=45)
         if j == cols-1:
             j = 0
@@ -92,7 +110,6 @@ def scatter_by_category(columns, df, category, number_columns=2):
     for column in columns:
         ax = fig.add_subplot(gs[i, j])
         g = df.plot.scatter(x=column, y=category, ax=ax)
-        g.set_xticklabels(g.get_xticklabels(),rotation=45)
         if j == cols-1:
             j = 0
             i+=1
